c07ac859316775b10f16019964f44e6ef26bc6b4
waozik <acl0676495@163.com>
2021-06-05 23:09:40 +0800

    commit

---
+++ 数据结构/查找最大子序列/查找最大子序列/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arrInt = new int[] { 1, 2, 3, 4, 5, 11, 33, 44, 55, -1, 2, -5 };
+            int summax = MaxSubSequenceSumFour(arrInt, 10);
+            Console.WriteLine(summax);
+            Console.ReadLine();
+        }
+        private static int MaxSubSequenceSumOne(int[] arr, int n) //O(n^3)
+        {
+            int sumMax = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    int sum = 0;
+                    for (int k = j; k <= i; k++)
+                    {
+                        sum += arr[k];
+                        if (sumMax < sum) sumMax = sum;
+                    }
+                }
+            }
+            return sumMax;
+        }
+
+        private static int MaxSubSequenceSumTwo(int[] arr, int n)//O(n^2)
+        {
+            int sumMax = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = i; j >= 0; j--)
+                {
+                    sum += arr[j];
+                    if (sum > sumMax) sumMax = sum;
+                }
+            }
+            return sumMax;
+        }
+
+        private static int MaxSubSequenceSumThree(int[] arr, int n) //O(n)
+        {
+            int sumMax = 0;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+                if (sumMax < sum) sumMax = sum;
+                if (sum < 0) sum = 0;
+
+            }
+            return sumMax;
+        }
+        private static int MaxSubSequenceSumFour(int[] arr, int n)
+        {
+           return BinaryMaxSum(arr, 0, n - 1);
+        }
+
+        private static int BinaryMaxSum(int[] arr, int left, int right)
+        {
+            int leftSumMax, rightSumMax;
+            int leftboardSumMax, rightboardSumMax;
+            int leftbordSum, rightboardSum;
+            int center, i = 0;
+            if (left == right)
+            {
+                if (arr[left] > 0) return arr[left];
+                else return 0;
+            }
+            center = (left + right) / 2;
+            leftSumMax = BinaryMaxSum(arr, left, center);
+            rightSumMax = BinaryMaxSum(arr, center + 1, right);
+            leftboardSumMax = 0;
+            leftbordSum = 0;
+            for (i = center; i >= left; i--)
+            {
+                leftbordSum += arr[i];
+                if (leftbordSum > leftboardSumMax) leftboardSumMax = leftbordSum;
+            }
+            rightboardSumMax = 0;
+            rightboardSum = 0;
+            for (i = center; i >= left; i--)
+            {
+                leftbordSum += arr[i];
+                if (rightboardSum > rightboardSumMax) rightboardSumMax = rightboardSum;
+            }
+          return  Math.Max(Math.Max(rightSumMax, leftSumMax), leftboardSumMax + rightboardSumMax);
+        }
+     
+
+    }
+}
---
+++ 数据结构/链表/链表/LinkedList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 链表
+{
+    public class LinkedList<T> where T : class
+    {
+        private Node<T> linkedList = new Node<T>();
+        public LinkedList<T> MakeEmpty()
+        {
+            linkedList = new Node<T>();
+            return this;
+        }
+
+        public void Add(T t)
+        {
+            //如果列表为空
+            if (linkedList.t == null)
+            {
+                this.linkedList.t = t;
+
+            }
+            else
+            {
+                Node<T> node = new Node<T>(t);
+                this.linkedList.nextNode = node;
+            }
+        }
+        public int Find(T t)
+        {
+            Node<T> node = this.linkedList;
+            int i = 1;
+            while (node.t != t && node.nextNode != null)
+            {
+                node = node.nextNode;
+                i++;
+            }
+            if (node.t == t)
+            {
+                return i;
+            }
+            else
+                return -1;
+        }
+
+        public int Insert(int index, T t)
+        {
+            if (index <= 0)
+                throw (new ArgumentException("参数index不能小于等于0"));
+            Node<T> n = new Node<T>(t);
+            Node<T> node = this.linkedList;
+            int i = 1;
+            while (node.nextNode != null && i < index)
+            {
+                node = node.nextNode;
+                i++;
+            }
+
+            //插入在首结点
+            if (i == 1)
+            {
+                n.nextNode = this.linkedList;
+                this.linkedList = n;
+            }
+            //跳出循环是因为插入的结点位置大于结点总数
+            else if (node.nextNode == null)
+            {
+                this.Add(t);
+            }
+            //插入的节点在 链表的中间
+            else
+            {
+                n.nextNode = this.linkedList.nextNode;
+                this.linkedList.nextNode = n;
+
+            }
+            return i;
+
+        }
+    }
+
+
+
+    public class Node<T> where T : class
+    {
+        public Node()
+        {
+            nextNode = null;
+            t = null;
+        }
+        public Node(T t)
+        {
+            this.nextNode = null;
+            this.t = t;
+        }
+        public Node<T> nextNode { get; set; }
+        public T t { get; set; }
+    }
+}
+
+
+
+
+
